bd22603f7641080268aa0b98b633581786c97a31
Jaqueline28 <jaqueline.abreuc@gmail.com>
2023-04-19 21:37:54 -0300

    Update conexao.php

--- php/conexao.php
+++ php/conexao.php
@@ -1,8 +1,8 @@
 <?php 
-$server = "localhost";
-$user = "root";
-$password = "";
-$dbname = "estacionamento";
+$server = "website-teste-server.mysql.database.azure.com";
+$user = "ufxcbrfnvi ";
+$password = "7421874RL7QKWF7K$";
+$dbname = "website-teste-database";
 
 $con = mysqli_connect($server, $user, $password, $dbname);
 ?>
